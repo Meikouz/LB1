7b2e1a195114ae1dc86d0f25673ffc5437854899
Meikouz <121649289+Meikouz@users.noreply.github.com>
2026-03-01 16:34:38 +0200

    Add files via upload

---
+++ ClientApp.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include "PhotoRobotAF.h"
+
+int main() {
+	setlocale(LC_ALL, "Ukr");
+
+    // Вибираємо фабрику "Female""Male"  vv
+    PhotoRobotFactory* factory = CreateMaleFactory();
+
+    Eyes* eyes = factory->createEyes();
+    Nose* nose = factory->createNose();
+    Mouth* mouth = factory->createMouth();
+
+    std::cout << "Фоторобот:\n";
+    std::cout << "- " << eyes->draw() << "\n";
+    std::cout << "- " << nose->draw() << "\n";
+    std::cout << "- " << mouth->draw() << "\n";
+
+    DestroyEyes(eyes);
+    DestroyNose(nose);
+    DestroyMouth(mouth);
+    DestroyFactory(factory);
+
+    return 0;
+}
